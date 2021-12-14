c7ef232078816b1cdffd213976fc7c32d8cc25c4
Yavuz Çelik <63187562+YavuzCelik-GitHub@users.noreply.github.com>
2021-12-14 13:36:47 +0300

    Update shell.h

--- include/shell.h
+++ include/shell.h
@@ -22,8 +22,8 @@ static int lineLength=0;
 static int ProcessCount=0;
 
 char *read_line(); //Girilen satırı okuyan ve döndüren fonksiyon
-char **split_line(char *);//kendisine gönderilen satırı ayırıcı karakterlere göre ayırıp bunları döndüren,argümanları tutan fonksiyon
-int execute(char **);//kendisine gönderilen argümanlar doğrultusunda yapması gereken işleri tamamlayan fonksiyon
+char **split_line(char *);//Kendisine gönderilen satırı ayırıcı karakterlere göre ayırıp bunları döndüren,argümanları tutan fonksiyon
+int execute(char **);//Kendisine gönderilen argümanlar doğrultusunda yapması gereken işleri tamamlayan fonksiyon
 int builtin_exit( );//Shell'den çıkmayı sağlayan fonksiyon
 int *process;
 int showpid();
