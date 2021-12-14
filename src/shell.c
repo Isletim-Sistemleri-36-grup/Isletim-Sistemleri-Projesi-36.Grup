0999df6b6c3f459968f881a73db85673decd5e4e
Emir Can Olgac <62564166+emircanolgac@users.noreply.github.com>
2021-12-14 12:55:20 +0300

    Update shell.c
    shell.c dosyasına read_line fonksiyonu eklendi

--- src/shell.c
+++ src/shell.c
@@ -14,4 +14,52 @@
 
 //**************************************************************
 
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define TOKEN_DELIM " \t\r\n"
+// SİL #define RESET "\e[0m"
+#define BUFF_SIZE 80    //Komut satırına girilen satırın 80 karakter sınırını taşıması adına tanımlandı
+#define TK_BUFF_SIZE 64
+#define processnumber 30
+
+//Renk Kodları
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
+#define reset "\e[0m"
+
 #include "shell.h"
+
+char *read_line() {  //shell'den girilen satırın okuma işlemini gerçekleştiriyor.
+  int buffsize = BUFF_SIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * buffsize); //satırı dinamik bir bellek bölgesi ayırdık
+  int c;
+ 
+  if (!buffer) { //yer ayrılmadıysa hata döndür
+    fprintf(stderr, "%sAllocation error%s\n", RED, reset);
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) { //her bir satırı oku ve belirli karakterlere geldiğinde \0 ekle ve bu satırı döndür
+    c = getchar();
+    if (c == EOF || c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+	lineLength++;   
+  }
+}
